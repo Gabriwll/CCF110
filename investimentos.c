50baa2fe644ebe4b7de9691dfd087b277aabcb44
Gabriwll <gabrielamorim.pessoal@gmail.com>
2023-09-08 13:59:26 -0300

    - Versão não testada
    - Adicionado:
    * Adicionar investimentos;
    * Excluir investimentos;
    * Editar investimentos;
    
    - Modificado:
    * Sistema de struct para cadastro de investimentos;
    
    - Próximos passos:
    * Aplicar a struct de investimentos nas funções;
    * Otimizar o algoritmo como um todo;

--- investimentos.c
+++ investimentos.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define n 20    //número de espaços para investimentos
 
 /*
 Encontrar uma forma de utilizar as funções e struct contidas nesse arquivo no main.
 */
 
 int investimentoEditor(){   //Essa função será disponível apenas para usuários da categoria administrador.
-    int edicao;
+    int edicao = 0, selectInvestimento;
+    struct investimentos investimento[n];
 
     printf("Escolha a ferramenta de edição: \n");
     printf("(1) Adicionar novo investimento;\n");
@@ -16,14 +20,52 @@ int investimentoEditor(){   //Essa função será disponível apenas para usuár
         scanf("%d", &edicao);
         switch (edicao){
         case 1:
-            
+            for(int i = 0; i < n; i++){
+                if(investimento[i].flag == 0){ //flag == 1 (espaço preenchido) //flag == 0 (espaço vago)
+                    printf("Digite o nome do investimento:\n");
+                    fgets(investimento[i].nome, sizeof(investimento[i].nome), stdin); //preenche o vetor de caracteres 'nome' contando com espaços
+
+                    printf("Defina o tipo de investimento:\n");
+                    printf("(1) Investimento de baixo risco.\n(2) Investimento de alto risco."); //possível bug quando usuário digita algo diferente de 1 ou 2
+                    scanf("%d", &investimento[i].tipo);                                          //fazer um get pra esse tipo de coisa
+
+                    printf("Defina a taxa e o rendimento (float):\n");
+                    scanf("%f%f", &investimento[i].taxa, &investimento[i].rendimento);
+
+                    investimento[i].flag = 1;
+                }
+            }
+
             break;
         
         case 2:
+            for(int i = 0; i < n; i++){
+                printf("Id: %d /nNome: %c/n", i, investimento[i].nome);
+            }
+
+            printf("\nInsira o id do investimento a ser removido:\n");
+            scanf("%d", &selectInvestimento);
+
+            /*
+            O restante das informações não precisa ser alterada uma vez que a variável que será mostra se o
+            investimento existe está setada em 0 (false).
+            */
+            investimento[selectInvestimento].flag = 0;
 
             break;
 
         case 3:
+            printf("Selecione o investimento:\n");
+            scanf("%d", &selectInvestimento);
+
+            printf("Defina o nome(string):\n");
+            scanf("%c", &investimento[selectInvestimento].nome);
+
+            printf("Defina o tipo(int '1 ou 2'):\n");               //possível bug quando usuário digita algo diferente de 1 ou 2
+            scanf("%d", &investimento[selectInvestimento].tipo);    //fazer um get pra esse tipo de coisa
+
+            printf("Defina a taxa e o rendimento(float):\n");
+            scanf("%f%f", &investimento[selectInvestimento].taxa, &investimento[selectInvestimento].rendimento);
 
             break;
         
@@ -34,14 +76,26 @@ int investimentoEditor(){   //Essa função será disponível apenas para usuár
     }
 }
 
-int investimentoPoupanca(int tempo, int capital){
+/*
+struct investimentos{
+    //id = posição no vetor
+    int tipo;   //baixoRisco = 1 && altoRisco = 2;    
+    char nome[30];
+    float rendimento;
+    int taxa;
+};
+*/
+
+int investimentoBaixoRisco(int tempo, int capital){
     int rendimentoLiquido[tempo], rendimentoBruto[tempo];
-    int rendimento = 0.01;  //o rendimento da poupanca será declarado nessa função por enquanto e será
-    int imposto = 0.01;     //considerado um rendimento de 1% == 1/100, tal qual o imposto sobre o rendimento;
+    float rendimento = 0.01;  //o rendimento da poupanca será declarado nessa função por enquanto e será
+                             //considerado um rendimento de 1% == 1/100, tal qual o imposto sobre o rendimento;
     
     for(int i = 0; i < tempo; i++){
+        float taxa = ((rendimento * capital) * 0.01);
+
         rendimentoBruto[i] = ((rendimento * capital) + capital);
-        rendimentoLiquido[i] = ((rendimento * capital) + capital) - ((rendimento * capital) * imposto); //tentar reduzir essa expressão
+        rendimentoLiquido[i] = (((rendimento * capital) + capital) - taxa);
 
         /*
         Líquido: rendimento obtido após o desconto das taxas;
@@ -53,9 +107,34 @@ int investimentoPoupanca(int tempo, int capital){
     //não sei se é possível retornar os vetores corretamente dessa forma na saída da função;
 }
 
-struct baixoRisco{  //a ideia aqui é criar um array de structs, desse modo, o usuário administrador fica
-    int id;         //livre para criar quantos investimentos ele desejar (limitado pelo tamanho do vetor)
+int investimentoAltoRisco(int tempo, int capital){
+    int rendimentoLiquido[tempo], rendimentoBruto[tempo];
+    int taxa = (capital / 100);
+    
+    for(int i = 0; i < tempo; i++){
+        int min = -10, max = 10;
+        float rendimento = (((min + rand()) + (max - min + 1)) / 100);
+        //Encontrar solução para o número aleatório gerado ser diferente a cada execução do sistema
+        
+        rendimentoBruto[i] = ((rendimento * capital) + capital);
+        rendimentoLiquido[i] = (((rendimento * capital) + capital) - taxa);
+    }
+
+    return rendimentoBruto, rendimentoLiquido;
+}
+
+/*
+    O modificador de rendimento dos investimentos de alto risco se dará por um valor pseudo-aleatório que
+    simulará as flutuações de valores de ações. Dessa forma, para fins de teste a função elaborada nessa
+    versão do projeto se baseia na função rand() da biblioteca <stdlib.h>. Essa função gera um número pseudo-
+    -aleatório com um intervalo definido nessa função de -10 a 10. Nesse caso o rendimento mensal pode variar
+    em 10% do valor bruto do capital investido.
+*/
+
+struct investimentos{
+    int flag; //id = posição no vetor
+    int tipo;   //baixoRisco = 1 && altoRisco = 2;    
     char nome[30];
-    int rendimento;
-    int taxa;
+    float rendimento;
+    float taxa;
 };
