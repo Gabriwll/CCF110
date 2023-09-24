a1917d0fd249e3a3f926a445746ffe96e240d237
Gabriwll <gabrielamorim.pessoal@gmail.com>
2023-09-24 18:21:48 -0300

    Sistema simples de rendimento
    Versão de elaboração de um sistema simples de rendimento, baseado num valor de rendimento não variável.
    Esse sistema já atribui o rendimento aos clientes previamente cadastrados.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include <string.h>
-#include <math.h>
 
 int main(){
     //cadastro do cleinte
@@ -20,7 +20,7 @@ int main(){
 
     switch(opcao) {
         case 1:
-            if(num_clientes < 100 ){
+            if(num_clientes <= 100){
             printf("Informe o nome do cliente: ");
             scanf("%s", nome_cliente[num_clientes]);
             printf("\n");
@@ -38,13 +38,14 @@ int main(){
         case 2:
             printf("CLIENTES CADASTRADOS: \n");
             for(int i = 0; i < num_clientes; i++){
-            printf("Nome: %s, Idade: %d\n" , nome_cliente[i], idade_cliente[i]);
+            printf("Id: %d Nome: %s, Idade: %d\n" , (i + 1), nome_cliente[i], idade_cliente[i]);
             }
+
             printf("\nTotal de clientes: %d\n" , num_clientes);
             break;
 
         case 3:
-            printf("em construção");
+            printf("em construção.\n");
             break;
 
         case 4:
@@ -57,10 +58,31 @@ int main(){
 
     }
     
-    
-    
     }
 
+    int cliente;
+    int tempo;
+    float capitalInical[100];
+
+    printf(" MENU DE INVESTIMENTO!\n");
+    printf("Selecione o cliente (id): \n");
+    scanf("%d", &cliente);
+
+    cliente--;
+
+    printf("Bem-vindo %s\n", nome_cliente[cliente]);
+    printf("\nInsira um capital inicial: \n");
+    scanf("%f", &capitalInical[cliente]);
+
+    printf("Defina o tempo de rendimento: \n");
+    scanf("%d", &tempo);
+
+    float rendimento[tempo];
+
+    for(int i = 0; i < tempo; i++){
+        rendimento[i] = (((capitalInical[cliente]) * 0.01) + capitalInical[cliente]);
+        capitalInical[cliente] = rendimento[i];
+    }
  
     return 0;
 }
