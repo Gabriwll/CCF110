25369cd0a9a567bcbcff0573527d2ec0949a8810
Gabriwll <gabrielamorim.pessoal@gmail.com>
2023-11-27 15:11:16 -0300

    Versão 1.1
    Novidades:
    - Elaboração da base do sistema de login;
    - Elaboração da base do sistema de leitura dos dados salvos no arquivo;
    
    Problemas:
    - A leitura do texto está com dificuldades para organizar as informações
    nas variáveis corretas na struct, é provável que tenhamos que tratar
    cada linha do arquivo com strtok();

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TIPO_EXECUCAO 1 //define o tipo do usuário (0 == adm e 1 == comum)
+/*Tipo de execução define se o sistema executará em um dos modos a seguir:
+* Usuário comum == 1:
+* - Acesso a cadastro, login e investimentos.
+* 
+* Usuário administrador == 0:
+* - Acesso a edição de investimentos disponíveis.
+*/
+#define TIPO_EXECUCAO 1
+
+/*
+Fazer um arquivo para essa funcionalidade talvez seria melhor, uma vez que o usuário alteraria o valor sem
+precisar abrir o código fonte
+*/
 
 /*Quantidade máxima de usuários a serem registrados no sistema.
 * 
 * Essa constante deverá ser substituida futuramente por um sistema de alocação dinâmica
 */
 #define MAX_USUARIO 50
-
-/*
-Fazer um arquivo para essa funcionalidade talvez seria melhor, uma vez que o usuário alteraria o valor sem
-precisar abrir o código fonte
-*/
 
 struct usuario{
     char nome[30];
@@ -25,16 +32,29 @@ int main(){
     FILE *arquivoClientes = NULL;
 
     struct usuario usuarios[MAX_USUARIO];
-    char nomeAux[30];
+    char nomeAux[30], senhaAux[8];
     char cadastroCliente;
 
-
-    /*Zerar os valores da struct "usuários" e preenchê-la de acordo com a base de dados
-    */
+    int flag;
 
     for(int i = 0; i < MAX_USUARIO; i++){
         usuarios[i].nome[0] = '\0';
     }
+
+    if((arquivoClientes = fopen("../Dados cadastrais/clientes.txt", "r")) == NULL){
+        printf("Erro ao acessar a base de dados de cadastro (R).\n");
+
+        return 1;
+    }
+
+    for(int i = 0; i < MAX_USUARIO; i++){
+        if(fscanf(arquivoClientes, "%s;%s;%d;%s;\n", &usuarios[i].nome, &usuarios[i].cpf, &usuarios[i].idade, &usuarios[i].senha) == EOF){
+            break;
+        }
+        printf("Usuario %d:\n%s\n%s\n%d\n%s\n", (i + 1), usuarios[i].nome, usuarios[i].cpf, usuarios[i].idade, usuarios[i].senha);
+    }
+
+    fclose(arquivoClientes);
 
     if(TIPO_EXECUCAO == 1){
         printf("Deseja fazer o login ou se cadastrar? ('L' = Login e 'C' = Cadastrar)\n");
@@ -42,20 +62,48 @@ int main(){
 
         switch(cadastroCliente){    //substituir futuramente por um sistema único que busca nos arquivos um usuário e caso não encontre pergunte se deseja cadastrar um novo cliente
             case 'L':
-                
+                flag = 1;
+
+                while (flag != 0){
+                    puts("Digite o seu nome:");
+                    scanf(" %s", &nomeAux);
+
+                    for(int i = 0; i < MAX_USUARIO; i++){
+                        if(nomeAux == usuarios[i].nome){
+                            puts("Digite a sua senha:");
+                            scanf(" %s", &senhaAux);
+
+                            if(senhaAux != usuarios[i].senha){
+                                puts("Senha incorreta. Tente novamente.");
+                                flag = 1;
+
+                                break;
+                            }
+
+                            flag = 0;
+
+                            break;
+                        }else if(usuarios[i].nome[0] == '\0'){
+                            printf("Usuario nao encontrado no sistema.\n Tente novamente.\n");
+                            flag = 1;
+                            
+                            break;
+                        }
+                    }
+                }
 
                 break;
 
             case 'C':
                 if((arquivoClientes = fopen("../Dados cadastrais/clientes.txt", "a")) == NULL){
-                    printf("Erro ao acessar a base de dados de cadastro.\n");
+                    printf("Erro ao acessar a base de dados de cadastro (A).\n");
 
                     return 1;
                 }
                 
                 //Para que o bloco a seguir funcione, os dados do arquivo devem preencher a struct a partir da linha 27
 
-                int flag = 1;
+                flag = 1;
 
                 while(flag != 0){
                     puts("Cadastro:\n");
